f9de614b786a274c99039350eb2c24efcc915039
Anastasia Khudoyarova <asya-iriska@yandex.ru>
2020-03-09 12:19:18 +0300

    Add checks and fixes

--- ht_3/src/com/akhudoyarova/ChessDesk.java
+++ ht_3/src/com/akhudoyarova/ChessDesk.java
@@ -4,18 +4,47 @@ public class ChessDesk {
 
     int x;
     int y;
+	
 
     public ChessDesk(int x, int y) {
-        if (x < 0 || x >= 8 || y < 0 || y >= 8)
-            throw new IllegalArgumentException("Invalid coordinates");
-        this.x = x;
-        this.y = y;
+		if (x < 0 || x >= 8 || y < 0 || y >= 8) {
+			throw new IllegalArgumentException("Invalid coordinates");
+		}
+       	this.x = x;
+       	this.y = y;
     }
+	
+	
+	public ChessDesk(String pos) {
+		String alph = "abcdefgh";
+       	char[] alphabet = alph.toCharArray();
+		char[] tmp = pos.toCharArray();
+
+		if (tmp.length != 2){
+			throw new IllegalArgumentException("Invalid coordinates");
+		}
+		
+		boolean flag = false;
+		for (int i = 0; i < alphabet.length; i++) {
+			if (tmp[0] == alphabet[i]) {
+				flag = true;
+				this.x = i;
+			}
+		}
+		if (!flag) {
+			throw new IllegalArgumentException("Invalid coordinates");
+		}
+		if (tmp[1] < '1' || tmp[1] > '8') {
+			throw new IllegalArgumentException("Invalid coordinates");
+		}
+		this.y = tmp[1] - '1';
+	}
+	
 
     @Override
     public String toString() {
-        String alph = "abcdefgh";
-        char[] alphabet = alph.toCharArray();
-        return String.valueOf(alphabet[this.x]) + String.valueOf(this.y + 1);
+		String alph = "abcdefgh";
+		char[] alphabet = alph.toCharArray();
+        return alphabet[this.x] + String.valueOf(this.y + 1);
     }
 }
--- ht_3/src/com/akhudoyarova/Main.java
+++ ht_3/src/com/akhudoyarova/Main.java
@@ -1,24 +1,52 @@
 package com.akhudoyarova;
 
+import com.akhudoyarova.except.IllegalMoveException;
 import java.util.Scanner;
+import static java.lang.Math.abs;
 
 public class Main {
+	
+	public static String check(ChessDesk[] coordinates) throws IllegalMoveException {
+//		ChessDesk start = coordinates[0];
+//		boolean flag = true;
+//		int index = 0;
+		for(int i = 0; i < coordinates.length - 1; i++) {
+			if (!((abs(coordinates[i + 1].x - coordinates[i].x) == 2 && abs(coordinates[i + 1].y - coordinates[i].y) == 1)
+					|| (abs(coordinates[i + 1].x - coordinates[i].x) == 1 && abs(coordinates[i + 1].y - coordinates[i].y) == 2))){
+//				flag = false;
+//				index = i;
+				throw new IllegalMoveException("Конь так не ходит:", coordinates[i].toString(), coordinates[i + 1].toString());
+			}
+//			start = coordinates[i];
+		}
+//		if (flag) {
+		return "OK";
+//		}
+//		else {
+//			throw new IllegalMoveException("Конь так не ходит:", coordinates[index].toString(), coordinates[index + 1].toString());
+//		}
+	}
 
-    //add convertation from <letter><number> to <number><number>
-//    public static void check(ChessDesk[] coordinates) {
-//
-//    }
 
-    public static void main(String[] args) {
-//	    ChessDesk element = new ChessDesk(7,3);
-//        System.out.println(element);
-        System.out.println("Enter amount of points");
-        Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
-        ChessDesk[] points = new ChessDesk[number];
+    public static void main(String[] args) throws IllegalMoveException {
+       	System.out.println("Enter amount of points");
+       	Scanner sc = new Scanner(System.in);
+       	int number = sc.nextInt();
 
-//        for (int i = 0; i < number; i++) {
-//            points[i] = new ChessDesk(1,2)
-//        }
+		System.out.println("Enter coordinates");
+		Scanner scStr= new Scanner(System.in);
+		String curPoints = scStr.nextLine();
+		String[] splitStr = curPoints.trim().split("\\s+");
+
+		if(splitStr.length != number){
+			throw new IllegalArgumentException("Amount of movements error");
+		}
+
+		ChessDesk[] points = new ChessDesk[number];
+		for (int i = 0; i < number; i++) {
+			points[i] = new ChessDesk(splitStr[i]);
+		}
+		
+		System.out.println(check(points));
     }
 }
